93c5f113776e5462e83e53172825a79bba19f38a
Ted Timmons <ted@timmons.me>
2018-10-14 17:45:02 -0700

    whitespace, style fixes, rm 'legacy' because we have a redir in place now

--- scrape.py
+++ scrape.py
@@ -33,46 +33,51 @@ import datetime
 import dateutil
 
 def item_to_rss(item):
-    link = "https://aws.amazon.com/"+item["id"].replace("#","/")+"/"
-    desc = ""
-    if "description" in item["additionalFields"]:
-        desc = item["additionalFields"]["description"]
-    elif "content" in item["additionalFields"]:
-        desc = item["additionalFields"]["content"]
-    return PyRSS2Gen.RSSItem(
-        author = item["author"],
-        title = item["name"],
-        link = link,
-        guid = link,
-        description = desc,
-        pubDate = dateutil.parser.parse(item["dateUpdated"])
-    )
+  link = "https://aws.amazon.com/" + item["id"].replace("#","/") + "/"
+  desc = ""
+  if "description" in item["additionalFields"]:
+    desc = item["additionalFields"]["description"]
+  elif "content" in item["additionalFields"]:
+    desc = item["additionalFields"]["content"]
+
+  return PyRSS2Gen.RSSItem(
+    author = item["author"],
+    title = item["name"],
+    link = link,
+    guid = link,
+    description = desc,
+    pubDate = dateutil.parser.parse(item["dateUpdated"])
+  )
 
 request = requests.get("https://aws.amazon.com/api/dirs/releasenotes/items?order_by=DateCreated&sort_ascending=false&limit=25&locale=en_US")
 
 if request.status_code == 500:
-    sys.exit(0)
+  sys.exit(0)
 
 rss = PyRSS2Gen.RSS2(
-    title = "Amazon AWS Release Notes scraped feed",
-    link = "https://aws.amazon.com/releasenotes/",
-    ttl = 6*60, # in minutes!
-    docs = "https://github.com/tedder/aws-release-notes-rss",
-    description = "new code and features from AWS",
-    lastBuildDate = datetime.datetime.now(),
-    items = map(item_to_rss, request.json()["items"])
+  title = "Amazon AWS Release Notes scraped feed",
+  link = "https://aws.amazon.com/releasenotes/",
+  ttl = 6*60, # in minutes!
+  docs = "https://github.com/tedder/aws-release-notes-rss",
+  description = "new code and features from AWS",
+  lastBuildDate = datetime.datetime.now(),
+  items = map(item_to_rss, request.json()["items"])
 )
 
 rssdata = rss.to_xml(request.encoding)
 
 
-if "S3_BUCKET_NAME" in os.environ:
-    s3 = boto3.client("s3")
-    s3.put_object(Bucket=os.environ["S3_BUCKET_NAME"], Key="rss/aws-release-notes.xml", Body=rssdata, ContentType="application/rss+xml", ContentEncoding=request.encoding, CacheControl="max-age=21600,public", ACL="public-read")
+if os.environ.get('S3_BUCKET_NAME'):
+  s3 = boto3.client("s3")
+  s3.put_object(
+    Bucket=os.environ["S3_BUCKET_NAME"],
+    Key="rss/aws-release-notes.xml",
+    Body=rssdata,
+    ContentType="application/rss+xml",
+    ContentEncoding=request.encoding,
+    CacheControl="max-age=21600,public",
+    ACL="public-read"
+  )
 
-    # also upload to legacy bucket:
-    if os.environ["S3_BUCKET_NAME"] == "dyn.tedder.me":
-        s3.put_object(Bucket="tedder", Key="rss/aws-release-notes.xml", Body='', ContentType="application/rss+xml", ContentEncoding=request.encoding, CacheControl="max-age=21600,public", ACL="public-read", WebsiteRedirectLocation="https://dyn.tedder.me/rss/aws-release-notes.xml")
-        print("uploaded to tedder")
 else:
-    print(rssdata)
+  print(rssdata)
